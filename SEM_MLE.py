dcdd151ed1688a94bd658c17842729b4fa30e27d
yusuke <yusuke.22360679@gmail.com>
2026-04-10 02:26:04 +0900

    Updating SEM_MLE.py

--- SEM_MLE.py
+++ SEM_MLE.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from semopy import Model
 
-# --- データの生成 (ご提示のコード) ---
+# データ生成
 np.random.seed(42)
 n = 10000
 z2 = np.random.normal(0, 1, n)
@@ -16,11 +16,8 @@ z4 = 0.7 * x + 0.7 * y + np.random.normal(0, 1, n)
 
 df = pd.DataFrame({'X':x, 'Y':y, 'Z1':z1, 'Z2':z2, 'Z3':z3, 'Z4':z4, 'Z5':z5, 'Z6':z6})
 
-# --- semopyによるパス解析 ---
-
-# 1. モデルの定義 (真のDAGを記述)
-# 演算子 '~' は回帰関係（因果の方向）を示します
-desc = """
+# 1.真のDAGを定義
+dag = """
     Z1 ~ Z2
     X ~ Z1 + Z5
     Z3 ~ X
@@ -28,11 +25,10 @@ desc = """
     Z4 ~ X + Y
 """
 
-# 2. モデルのインスタンス化と推定
-model = Model(desc)
+# 2. 推定
+model = Model(dag)
 model.fit(df)
 
 # 3. 推定結果の表示
-# Estimateカラムが推定されたパス係数です
 estimates = model.inspect()
-print(estimates[['lval', 'op', 'rval', 'Estimate', 'Std. Err', 'z-value', 'p-value']])
+print(estimates[['lval', 'op', 'rval', 'Estimate']])
